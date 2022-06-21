69e85e9081601ba353fc1efc2905c2dabd6a0914
cs1912102 <92505374+cs1912102@users.noreply.github.com>
2022-06-21 16:45:38 +0500

    Update CourseSheet.js

--- src/components/CourseSheet.js
+++ src/components/CourseSheet.js
@@ -1,35 +1,42 @@
 import React from "react";
+import courses from "./listCourses";
+import AutoCompleteText from "./AutoCompleteText";
 
 const CourseSheet = () => {
-  //   let sems = [
-  //     { sno: 1, name: "1st" },
-  //     { sno: 2, name: "2nd" },
-  //     { sno: 3, name: "3rd" },
-  //     { sno: 4, name: "4th" },
-  //     { sno: 5, name: "5th" },
-  //     { sno: 6, name: "6th" },
-  //     { sno: 7, name: "7th" },
-  //     { sno: 8, name: "8th" },
-  //   ];
+    let sems = [
+      { sno: 1, name: "1st" },
+      { sno: 2, name: "2nd" },
+      { sno: 3, name: "3rd" },
+      { sno: 4, name: "4th" },
+      { sno: 5, name: "5th" },
+      { sno: 6, name: "6th" },
+      { sno: 7, name: "7th" },
+      { sno: 8, name: "8th" },
+    ];
   return (
     <div>
       <table>
-        <tbody>
+      <tbody>
+          
           <tr>
             <th style={{ width: "75px" }}>Code</th>
             <th style={{ width: "450px" }}>Title</th>
             <th style={{ width: "25px", textAlign: "center" }}>Cr</th>
           </tr>
-          <tr>
-            <th colspan="3" style={{ fontWeight: "bold" }}>
-              1st Semester
-            </th>
-          </tr>
-          <tr>
-            <td></td>
-            <td style={{ height: "25px" }}></td>
-            <td></td>
-          </tr>
+          {sems.map((c) => {
+            return (
+            <><tr>
+              <th></th>
+              <th colspan="3" style={{ fontWeight: "bold" }}>
+                {c.name} Semester
+              </th>
+            </tr>
+            <tr>
+              <td></td>
+              <td style={{ height: "25px" }}><AutoCompleteText items={courses.filter((co)=>(co.semester===c.sno))} /></td>
+              <td>  </td>
+            </tr></>
+            );})}
         </tbody>
       </table>
     </div>
