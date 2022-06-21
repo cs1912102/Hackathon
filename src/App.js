0f0c1d2f9ba6c69dbfc568b0879aee8fbae21189
cs1912102 <92505374+cs1912102@users.noreply.github.com>
2022-06-21 16:46:16 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import "./App.css";
 import CourseSheet from "./components/CourseSheet";
-import AutoCompleteText from "./components/AutoCompleteText";
-import courses from "./components/listCourses.js";
 
 // import HideableText from'./HideableText';
 
 function App() {
   return (
     <div className="App">
-      <AutoCompleteText items={courses} />
       <CourseSheet />
     </div>
   );
